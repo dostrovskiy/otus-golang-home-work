6acb262e95b4d8919aa11b047884a170cbef320d
dostrovskiy <dostrovskiy@diasoft.ru>
2024-08-28 09:55:45 +0300

    Added golang.org/x/example/hello/reverse package; added Hello, OTUS! reversed string printing.

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,14 @@
+/*
+Main package with main func printing reverse string
+*/
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	fmt.Println(reverse.String("Hello, OTUS!"))
 }
