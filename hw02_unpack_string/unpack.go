b06aee7e1fe5ba8b375c375af1e38de87b23a975
dostrovskiy <dostrovskiy@diasoft.ru>
2024-09-07 19:59:12 +0300

    Implemented Unpack function; errors testing are placed in a separate file; added .golangci.yml

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,64 @@
+/*
+Package hw02unpackstring unpack string with format <char><number> will be replaced with <char><char><char>... number times, e.g. "a4bc2d5e" -> "aaaabccddddde"
+*/
 package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strings"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var errorInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+/*
+ErrorStringContainsSeveralDigitsInRow is returned when string contains several digits in a row, e.g. "aaa10b".
+*/
+var ErrorStringContainsSeveralDigitsInRow = errors.New("string contains several digits in a row")
+
+/*
+ErrorStringStartsWithNumber is returned when string starts with number, e.g. "3abc".
+*/
+var ErrorStringStartsWithNumber = errors.New("string starts with number")
+
+func runeToNumber(r rune) (int, error) {
+	if !unicode.IsDigit(r) {
+		return 0, fmt.Errorf("rune is not a digit")
+	}
+	return int(r - '0'), nil
+}
+
+/*
+Unpack string with format <char><number> will be replaced with <char><char><char>... number times, e.g. "a4bc2d5e" -> "aaaabccddddde"
+*/
+func Unpack(s string) (string, error) {
+	if strings.TrimSpace(s) == "" {
+		return "", nil
+	}
+	var runes = []rune(s)
+	if _, err := runeToNumber(runes[0]); err == nil {
+		return "", ErrorStringStartsWithNumber
+	}
+	var sb strings.Builder
+	var nextNum int
+	var nextErr error
+	for i, r := range runes {
+		nextNum, nextErr = 0, errorInvalidString
+		if i < len(runes)-1 {
+			nextNum, nextErr = runeToNumber(runes[i+1])
+		}
+		if _, err := runeToNumber(r); err == nil {
+			if nextErr == nil {
+				return "", ErrorStringContainsSeveralDigitsInRow
+			}
+			continue
+		}		
+		if nextErr == nil {
+			sb.WriteString(strings.Repeat(string(r), nextNum))
+		} else {
+			sb.WriteString(string(r))
+		}
+	}
+	return sb.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -1,7 +1,6 @@
 package hw02unpackstring
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,6 +15,7 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "象2形 5文字1", expected: "象象形     文字"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -29,17 +29,6 @@ func TestUnpack(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
 			require.Equal(t, tc.expected, result)
-		})
-	}
-}
-
-func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
-	for _, tc := range invalidStrings {
-		tc := tc
-		t.Run(tc, func(t *testing.T) {
-			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
 		})
 	}
 }
