6b4d3d53e0ea304e019988cb4582e4fb19ad2eba
dostrovskiy <dostrovskiy@diasoft.ru>
2024-10-26 16:22:41 +0300

    Pipeline execution without done.

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,14 +1,26 @@
+// Package hw06pipelineexecution provides a pipeline execution function
 package hw06pipelineexecution
 
 type (
-	In  = <-chan interface{}
+	// In is a channel for input data for pipeline stage.
+	In = <-chan interface{}
+	// Out is a channel for output data for pipeline stage.
 	Out = In
-	Bi  = chan interface{}
+	// Bi is a bidirectional channel.
+	Bi = chan interface{}
 )
 
+// Stage is a pipeline stage function.
 type Stage func(in In) (out Out)
 
+// ExecutePipeline starts pipeline executing stages in order.
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	// Выстраиваем пайплайн: для каждой стадии создаем горутину, которая слушает исходящий канал от предыдущей стадии
+	// Входящий канал для первой: входящий in функции ExecutePipeline
+	// Исходящий канал для последней, out, возвращаем из функции ExecutePipeline
+	out := in
+	for _, stage := range stages {
+		out = stage(out)
+	}
+	return out
 }
