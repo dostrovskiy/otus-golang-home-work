1a43b68c8477ce6025b797b28c38ba43089c33e2
dostrovskiy <dostrovskiy@diasoft.ru>
2024-11-21 14:29:40 +0300

    Implemented env_reader

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,21 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("simple read env from dir", func(t *testing.T) {
+		envs, err := ReadDir("testdata/env")
+		require.NoError(t, err)
+		require.Equal(t, Environment{
+			"BAR":   EnvValue{"bar", false},
+			"EMPTY": EnvValue{"", false},
+			"FOO":   EnvValue{"   foo\nwith new line", false},
+			"HELLO": EnvValue{"\"hello\"", false},
+			"UNSET": EnvValue{"", true},
+		}, envs)
+	})
 }
